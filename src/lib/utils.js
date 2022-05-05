cda1ac4c142ff004fdcaf1fe5dd2188b50773c74
Stephen Hutchings <stephen@s-ings.com>
2022-05-06 00:53:09 +1000

    Chore: Better code documentation of utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,17 +1,43 @@
 const DEFAULT_PRECISION = Math.ceil(Math.abs(Math.log10(Number.EPSILON)))
 
-const sum = (a) => a.reduce((m, v) => m + (v.value || v), 0)
+/**
+ * Add the values in an array, reading `item.value` where present.
+ * @private
+ * @param {Array} array
+ * @returns {number} total
+ */
+const sum = (array) => array.reduce((m, v) => m + (v.value || v), 0)
 
-const percent = (v) => +(v * 100).toFixed(2) + "%"
+/**
+ * Convert a value between 0 and 1 to a percentage string.
+ * @private
+ * @param {number} value
+ * @param {string} [units]
+ * @returns {string} percent
+ */
+const percent = (value, units = "%") => +(value * 100).toFixed(2) + units
 
-const toPrecision = (v, precision = DEFAULT_PRECISION) => {
+/**
+ * Convert a value to a specified precision to avoid floating point errors
+ * @private
+ * @param {number} value
+ * @param {number} [precision]
+ * @returns {number} value
+ */
+const toPrecision = (value, precision = DEFAULT_PRECISION) => {
   const f = Math.pow(10, precision)
-  const n = precision < 0 ? v : 0.01 / f + v
+  const n = precision < 0 ? value : 0.01 / f + value
   return Math.round(n * f) / f
 }
 
-const decimalPlaces = (n) => {
-  const s = (+n).toString()
+/**
+ * Counts the number of decimal places specified by a number
+ * @private
+ * @param {number} value
+ * @returns {number} decimals places
+ */
+const decimalPlaces = (value) => {
+  const s = (+value).toString()
   const ie = s.indexOf("e")
   const id = s.indexOf(".")
 
