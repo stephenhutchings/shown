e3740d95845a9324e4cc5e4a6671f302c13de6bd
Stephen Hutchings <stephen@s-ings.com>
2022-05-12 11:00:20 +1000

    Fix: #1 Sum should handle falsey values correctly

--- test/lib/utils.test.js
+++ test/lib/utils.test.js
@@ -3,7 +3,10 @@ import utils from "../../src/lib/utils.js"
 describe("utils", () => {
   test("to sum an array", () => {
     expect(utils.sum([1, 2, 3])).toBe(6)
+    expect(utils.sum([0, 1, 2])).toBe(3)
     expect(utils.sum([{ value: 2 }])).toBe(2)
+    expect(utils.sum([{ value: 2 }, { value: 0 }])).toBe(2)
+    expect(utils.sum([undefined, { value: false }, 1])).toBe(1)
   })
 
   test("to convert to a percent", () => {
